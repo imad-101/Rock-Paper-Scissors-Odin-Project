1b682ff3be4fc7fd5bd5f043aa41eed7465d3bbb
imad-101 <imaduddin10.1973@gmail.com>
2024-07-07 13:08:28 +0500

    used onclick event on buttons to pass parameters to the function

---
+++ script.js
@@ -0,0 +1,9 @@
+const options = ['rock' , 'paper' , 'scissor']
+
+function getComputerChoice(){
+    const choice = Math.floor [Math.random() * options.length]
+    return choice
+}
+
+getComputerChoice();
+
